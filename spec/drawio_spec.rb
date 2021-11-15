428727b602f8d9d9a654b400ebb804fc3405ab86
Lionel Molinier <lionel@molinier.eu>
2021-11-15 18:45:34 +0100

    Resolve name conflict

--- spec/drawio_spec.rb
+++ spec/drawio_spec.rb
@@ -1,6 +1,6 @@
 require_relative 'test_helper'
 
-CODE = <<-eos
+DRAWIO_DIAGRAM = <<-eos
 <mxfile host="Electron" modified="2021-11-15T13:32:35.319Z" agent="5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) draw.io/15.7.3 Chrome/91.0.4472.164 Electron/13.6.1 Safari/537.36" etag="7Kwj1Dk9rrArAG1jnUv6" compressed="false" version="15.7.3" type="device" pages="2">
   <diagram id="39dSL4geXcCnPDmTARuJ" name="Page-1">
     <mxGraphModel dx="1102" dy="857" grid="1" gridSize="10" guides="1" tooltips="1" connect="1" arrows="1" fold="1" page="1" pageScale="1" pageWidth="827" pageHeight="1169" math="0" shadow="0">
@@ -32,15 +32,15 @@ CODE = <<-eos
 eos
 
 describe Asciidoctor::Diagram::DrawioInlineMacroProcessor do
-  include_examples "inline_macro", :drawio, CODE, [:png, :svg, :pdf]
+  include_examples "inline_macro", :drawio, DRAWIO_DIAGRAM, [:png, :svg, :pdf]
 end
 
 describe Asciidoctor::Diagram::DrawioBlockMacroProcessor do
-  include_examples "block_macro", :drawio, CODE, [:png, :svg, :pdf]
+  include_examples "block_macro", :drawio, DRAWIO_DIAGRAM, [:png, :svg, :pdf]
 end
 
 describe Asciidoctor::Diagram::DrawioBlockProcessor do
-  include_examples "block", :drawio, CODE, [:png, :svg, :pdf]
+  include_examples "block", :drawio, DRAWIO_DIAGRAM, [:png, :svg, :pdf]
 
   it "should support drawio options as attributes" do
     doc = <<-eos
@@ -52,7 +52,7 @@ Doc Writer <doc@example.com>
 
 [drawio, crop=true]
     eos
-    doc << CODE
+    doc << DRAWIO_DIAGRAM
 
     d = load_asciidoc doc
     expect(d).to_not be_nil
